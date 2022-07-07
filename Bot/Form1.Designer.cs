662ba1614edbc0230ed30ab66f50f6ff01922526
Burak Kalkan <burak.kalkan26@gmail.com>
2022-07-07 18:31:24 +0300

    Add project files.

---
+++ Bot/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeSpanEdit1 = new DevExpress.XtraEditors.TimeSpanEdit();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.timeSpanEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "BAŞLAT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeSpanEdit1
+            // 
+            this.timeSpanEdit1.EditValue = System.TimeSpan.Parse("00:00:00");
+            this.timeSpanEdit1.Location = new System.Drawing.Point(152, 19);
+            this.timeSpanEdit1.Name = "timeSpanEdit1";
+            this.timeSpanEdit1.Properties.AllowEditMilliseconds = true;
+            this.timeSpanEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.timeSpanEdit1.Size = new System.Drawing.Size(113, 20);
+            this.timeSpanEdit1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mouse Click atılacak saat :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Erken bas :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 47);
+            this.textBox1.MaxLength = 5;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "986";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(115, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ms";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(233, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DURDUR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 77);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.timeSpanEdit1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mouse Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.timeSpanEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraEditors.TimeSpanEdit timeSpanEdit1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Bot/Form1.cs
@@ -0,0 +1,117 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+namespace Bot
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+        public Form1()
+        {
+            InitializeComponent();
+            timeSpanEdit1.EditValue = DateTime.Now.TimeOfDay;
+        }
+
+        DateTime baslangic, bitis;
+        TimeSpan fark;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            baslangic = DateTime.Now.AddMilliseconds(Convert.ToInt16(textBox1.Text));
+            fark = bitis - baslangic;
+            if (baslangic >= bitis)
+            {
+                timer1.Stop();
+                this.Text = "00:00:00.0000000";
+                mouse_event(MOUSEEVENTF_LEFTDOWN, Control.MousePosition.X, Control.MousePosition.Y, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, Control.MousePosition.X, Control.MousePosition.Y, 0, 0);
+                MessageBox.Show(DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second + "." + DateTime.Now.Millisecond, "Click Zamanı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox1.Enabled = true;
+            }
+            else
+            {
+                this.Text = fark.ToString();
+                textBox1.Enabled = false;
+            }
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (msg.WParam.ToInt32())
+            {
+                case (int)Keys.F1:
+                    timer1.Stop();
+                    textBox1.Enabled = true;
+                    break;
+                case (int)Keys.F2:
+                    baslangic = DateTime.Now;
+                    bitis = DateTime.Today + timeSpanEdit1.TimeSpan;
+                    if (bitis > baslangic)
+                    {
+                        timer1.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Belirlediğiniz Click saati geçti.", "Dikkat!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    break;
+                case (int)Keys.Escape:
+                    this.Close();
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "0";
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            textBox1.Enabled = true;
+        }
+        private void CreateShortcut()
+        {
+            object shDesktop = (object)"Desktop";
+            WshShell shell = new WshShell();
+            string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\MouseClicker.lnk";
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.Description = "Mouse Clicker Kısayolu";
+            shortcut.TargetPath = Application.StartupPath.Trim()+ @"\Bot.exe";
+            shortcut.Save();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateShortcut();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baslangic = DateTime.Now;
+            bitis = DateTime.Today + timeSpanEdit1.TimeSpan;
+            if (bitis > baslangic)
+            {
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Belirlediğiniz Click saati geçti.", "Dikkat!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+    }
+}
